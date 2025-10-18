888e9aa1ca0ce37a84fe1bff0f83d0b9a16177c8
anantjha320 <anantjha0000@gmail.com>
2025-10-18 22:53:42 +0530

    init commit

--- src/coding.java
+++ src/coding.java
@@ -1,8 +1,8 @@
-public class coding {
-    public static void Main(String[] args){
+  public class coding {
+    public static void main(String[] args){
         //Variables
-        String name "Tony Stark";
-        int age ="18";
+        String name = "Tony Stark";
+        int age =18;
         double price = 25.25;
         int a = 25;
         int b =10;
