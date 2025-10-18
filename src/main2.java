ce55ca5b2b934cef671c5f851143bd6edee18f84
anantjha320 <anantjha0000@gmail.com>
2025-10-18 23:36:10 +0530

    init commit

--- src/main2.java
+++ src/main2.java
@@ -1,8 +1,15 @@
+package com.apnacollege;
+import java.util.*;
+
 public class main2{
     public static void main(String[] args){
         int a = 10;
          int b= 20;
          int sum=a+b;
         System.out.println(sum);
+        //input
+         Scanner sc =new Scanner(System.in);
+         String name=sc.nextLine();
+        System.out.println(name);
     }
 }
